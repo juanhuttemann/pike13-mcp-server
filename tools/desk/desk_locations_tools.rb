391865196748fd5e055b9a54e90e070731de58ae
Juan Huttemann <juanfhuttemann@gmail.com>
2025-11-02 11:20:27 -0300

    re organize locations tools

--- tools/desk/desk_locations_tools.rb
+++ tools/desk/desk_locations_tools.rb
@@ -1,46 +1,6 @@
 # frozen_string_literal: true
 
-require_relative 'base_tool'
-
-# Front (CLIENT) Location Tools
-
-class FrontListLocations < Pike13BaseTool
-  description <<~DESC
-    [CLIENT] List public business locations.
-    Returns locations with name, address, timezone, phone, and hours.
-    Use to display location options to customers for booking or finding addresses.
-    Shows only publicly visible locations.
-  DESC
-
-  class << self
-    def call(server_context:)
-      Pike13::Front::Location.all.to_json
-    end
-  end
-end
-
-class FrontGetLocation < Pike13BaseTool
-  description <<~DESC
-    [CLIENT] Get specific location by ID.
-    Returns location details including address, hours, contact info, and timezone.
-    Use to show detailed location information to customers.
-  DESC
-
-  input_schema(
-    properties: {
-      location_id: { type: 'integer', description: 'Unique Pike13 location ID' }
-    },
-    required: ['location_id']
-  )
-
-  class << self
-    def call(location_id:, server_context:)
-      Pike13::Front::Location.find(location_id).to_json
-    end
-  end
-end
-
-# Desk (STAFF) Location Tools
+require_relative "../base_tool"
 
 class DeskListLocations < Pike13BaseTool
   description <<~DESC
---
+++ tools/front/front_locations_tools.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require_relative 'base_tool'
+
+class FrontListLocations < Pike13BaseTool
+  description <<~DESC
+    [CLIENT] List public business locations.
+    Returns locations with name, address, timezone, phone, and hours.
+    Use to display location options to customers for booking or finding addresses.
+    Shows only publicly visible locations.
+  DESC
+
+  class << self
+    def call(server_context:)
+      Pike13::Front::Location.all.to_json
+    end
+  end
+end
+
+class FrontGetLocation < Pike13BaseTool
+  description <<~DESC
+    [CLIENT] Get specific location by ID.
+    Returns location details including address, hours, contact info, and timezone.
+    Use to show detailed location information to customers.
+  DESC
+
+  input_schema(
+    properties: {
+      location_id: { type: 'integer', description: 'Unique Pike13 location ID' }
+    },
+    required: ['location_id']
+  )
+
+  class << self
+    def call(location_id:, server_context:)
+      Pike13::Front::Location.find(location_id).to_json
+    end
+  end
+end
