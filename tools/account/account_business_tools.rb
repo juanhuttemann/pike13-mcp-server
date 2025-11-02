d75b08f5982c0ad9f9acc2ac4748ae93427fa990
Juan Huttemann <juanfhuttemann@gmail.com>
2025-11-02 13:59:56 -0300

    remove AccountGetBusiness tool

--- tools/account/account_business_tools.rb
+++ tools/account/account_business_tools.rb
@@ -15,24 +15,3 @@ class AccountListBusinesses < Pike13BaseTool
     end
   end
 end
-
-class AccountGetBusiness < Pike13BaseTool
-  description <<~DESC
-    [ACCOUNT] Get specific business details by ID.
-    Returns business object with name, subdomain, timezone, settings.
-    Use when user needs details about a specific business they own or administer.
-  DESC
-
-  input_schema(
-    properties: {
-      business_id: { type: 'integer', description: 'Unique Pike13 business ID' }
-    },
-    required: ['business_id']
-  )
-
-  class << self
-    def call(business_id:, server_context:)
-      Pike13::Account::Business.find(business_id).to_json
-    end
-  end
-end
